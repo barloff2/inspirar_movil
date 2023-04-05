5fccf210b64f4a1fad35b7d10cd9fcef9eb19694
Mitzy Mo <mitzy.93@gmail.com>
2023-04-04 20:39:03 -0500

    "demo up to 7.14"

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-import 'chat_message.dart';
-import 'three_dots.dart';
-
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
 
@@ -14,10 +11,52 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _controller = TextEditingController();
+
+  Widget _buildTextComposer() {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _controller,
+            decoration:
+                const InputDecoration.collapsed(hintText: "Ask Something"),
+          ),
+        ),
+        ButtonBar(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.send),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ],
+    ).px16();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("AI Chat DEMO")),
+      appBar: AppBar(
+        title: Text("Hello World"),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(child: ListView.builder(itemBuilder: (context, index) {
+              return Container(
+                height: 100,
+                color: Color.fromARGB(255, 150, 216, 252),
+              );
+            })),
+            Container(
+              decoration: BoxDecoration(),
+              child: _buildTextComposer(),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
+import 'package:flutter/material.dart';
 import 'chat_screen.dart';
 
 Future main() async {
