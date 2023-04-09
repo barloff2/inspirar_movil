482c138b2ed61b85d72e2d697f512ef6ab410626
Mitzy Mo <mitzy.93@gmail.com>
2023-04-09 09:28:13 -0500

    "creating the user class"

---
+++ lib/models/usuario.dart
@@ -0,0 +1,24 @@
+class Usuario {
+  int id;
+  String name;
+  //String lastName;
+  //String login;
+  //String password;
+  String phone;
+  //String email;
+  //String tarjProf;
+  //String reTHUS;
+
+  Usuario({required this.id, required this.name, required this.phone});
+
+  /*Usuario(
+      {this.id,
+      this.name,
+      this.lastName,
+      this.login,
+      this.password,
+      this.phone,
+      this.email,
+      this.tarjProf,
+      this.reTHUS});*/
+}
