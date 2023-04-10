4f5b13d829c3c550cf3fa45073cfb40ada0dffcd
Mitzy <80993235+MitzyMo@users.noreply.github.com>
2023-04-09 21:28:39 -0500

    Adding the Database manager

---
+++ lib/utils/database_helper.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+import 'package:inspirar_movil/models/contact.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static const _databaseName = 'ContactDatabase.db';
+  static const _databaseVersion = 1;
+
+  //singleton class, it means it can only be initiated once.
+  DatabaseHelper._();
+  static final instance = DatabaseHelper._();
+
+  Database _database;
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  _initDatabase() async {
+    Directory dataDirectory = await getApplicationDocumentsDirectory();
+    String dbPath = join(dataDirectory.path, _databaseName);
+    print(dbPath);
+    return await openDatabase(dbPath,
+        version: _databaseVersion, onCreate: _onCreateDB);
+  }
+
+  Future _onCreateDB(Database db, int version) async {
+    //create tables
+    await db.execute('''
+      CREATE TABLE ${Contact.tblContact}(
+        ${Contact.colId} INTEGER PRIMARY KEY AUTOINCREMENT,
+        ${Contact.colName} TEXT NOT NULL,
+        ${Contact.colMobile} TEXT NOT NULL
+      )
+      ''');
+  }
+
+//contact - insert
+  Future<int> insertContact(Contact contact) async {
+    Database db = await database;
+    return await db.insert(Contact.tblContact, contact.toMap());
+  }
+
+//contact - update
+  Future<int> updateContact(Contact contact) async {
+    Database db = await database;
+    return await db.update(Contact.tblContact, contact.toMap(),
+        where: '${Contact.colId}=?', whereArgs: [contact.id]);
+  }
+
+//contact - delete
+  Future<int> deleteContact(int id) async {
+    Database db = await database;
+    return await db.delete(Contact.tblContact,
+        where: '${Contact.colId}=?', whereArgs: [id]);
+  }
+
+//contact - retrieve all
+  Future<List<Contact>> fetchContacts() async {
+    Database db = await database;
+    List<Map> contacts = await db.query(Contact.tblContact);
+    return contacts.length == 0
+        ? []
+        : contacts.map((e) => Contact.fromMap(e)).toList();
+  }
+}
