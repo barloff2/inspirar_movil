0d63db10757c395ce165b1c8be9a25d145f08e85
Mitzy <80993235+MitzyMo@users.noreply.github.com>
2023-04-09 21:23:02 -0500

    "Updating Main class"

---
+++ lib/models/contact.dart
@@ -0,0 +1,26 @@
+class Contact {
+  static const tblContact = 'contacts';
+  static const colId = 'id';
+  static const colName = 'name';
+  static const colMobile = 'mobile';
+
+  Contact({this.id, this.name, this.mobile});
+
+  int id;
+  String name;
+  String mobile;
+
+  Contact.fromMap(Map<String, dynamic> map) {
+    id = map[colId];
+    name = map[colName];
+    mobile = map[colMobile];
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{colName: name, colMobile: mobile};
+    if (id != null) {
+      map[colId] = id;
+    }
+    return map;
+  }
+}
